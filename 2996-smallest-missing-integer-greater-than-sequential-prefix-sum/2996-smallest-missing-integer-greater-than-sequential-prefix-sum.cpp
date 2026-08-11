a643b06e0a7d8bccd72502f52f828e336c7d27ef
abhinavmehta7 <abhinavmehtaoriginal@gmail.com>
2026-08-11 19:41:06 +0530

    Time: 0 ms (100%), Space: 22.5 MB (77.12%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int missingInteger(vector<int>& nums) {
-        int n=nums.size(),sum=nums[0];
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]+1) sum+=nums[i];
-            else break;
+        int sum = nums[0];
+        bitset<52> seen;
+        bool seq = true;
+        seen.set(nums[0]);
+        for (int i = 1; i < nums.size(); i++) {
+            if (seq && nums[i] == nums[i - 1] + 1) sum += nums[i];
+            else {
+                seq = false;
+                if (sum > 50)
+                    return sum;
+            }
+            seen.set(nums[i]);
         }
-        unordered_set<int> s(nums.begin(),nums.end());
-        while (s.count(sum))
-            sum++;
+        for (int i = sum; i < 52; i++)
+            if (!seen.test(i))
+                return i;
         return sum;
     }
 };
