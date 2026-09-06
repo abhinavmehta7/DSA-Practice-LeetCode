1e5bdaf69aacd90cd765e9d7c944f60248a57a17
abhinavmehta7 <abhinavmehtaoriginal@gmail.com>
2026-09-06 23:15:17 +0530

    Time: 28 ms (63.03%), Space: 44.3 MB (31.09%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long fact(long long n){
+        if(n==0 || n==1) return 1;
+        return n*fact(n-1);
+    }
+    long long pnc(long long p,long long r){
+        return fact(p)/(fact(r)*fact(p-r));//p>=r
+    }
+    int numDistinct(string s, string t) {
+        int m = s.length(), n = t.length();
+        if (m < n) {
+            return 0;
+        }
+        vector<vector<unsigned long long>> dp(m + 1, vector<unsigned long long>(n + 1));
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            char sChar = s.at(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char tChar = t.at(j);
+                if (sChar == tChar) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
